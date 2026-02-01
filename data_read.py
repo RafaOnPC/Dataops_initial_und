d13e4b680a9d61b673abd40e0649af2faff2fdd0
Rafa <YamiYugi2223@outlook.com>
2026-02-01 02:33:04 -0500

    Adding a dynamic route for data read

--- data_read.py
+++ data_read.py
@@ -2,10 +2,20 @@ import pandas as pd
 import glob
 import os
 
-# Leer archivo txt
-
 def cargar_datos():
-    archivo_txt = 'C:\\Users\\rafae\\OneDrive\\Desktop\\Pipeline-Jenkins\\Datos\\unidad_01_essalud.txt'
-    df = pd.read_csv(archivo_txt, delimiter='|', dtype=str)
-    print(f"Lectura exitosa: {archivo_txt}")
-    return df
+    ruta_datos = os.path.join('Datos', '*.txt')
+    archivos = glob.glob(ruta_datos)
+
+    if not archivos:
+        raise FileNotFoundError('No se encontraron archivos TXT en la carpeta Datos')
+
+    lista_df = []
+
+    for archivo in archivos:
+        df = pd.read_csv(archivo, delimiter='|', dtype=str)
+        df['archivo_origen'] = os.path.basename(archivo)
+        lista_df.append(df)
+        print(f'Lectura exitosa: {archivo}')
+
+    df_final = pd.concat(lista_df, ignore_index=True)
+    return df_final
